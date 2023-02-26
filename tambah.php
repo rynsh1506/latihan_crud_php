54700dbd1900023df6299bd0e1c442e3d4896d80
MuhammadRiyansyah <111970976+rynsh1506@users.noreply.github.com>
2023-02-26 12:16:40 -0800

    benerin tag html

--- tambah.php
+++ tambah.php
@@ -8,7 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>TAMBAH</title>
 </head>
-
+    
+<html>
+    
 <body>
     <div class="vh-100 d-flex align-items-center justify-content-center flex-column">
         <h1>Tambah Mahasiswa</h1>
