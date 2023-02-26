865d62013c4696c7af1db67b8c9e354025155ef0
MuhammadRiyansyah <111970976+rynsh1506@users.noreply.github.com>
2023-02-26 12:22:07 -0800

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -9,26 +9,24 @@
     <title>TAMBAH</title>
 </head>
     
-<html>
-    
-    <body>
-        <div class="vh-100 d-flex align-items-center justify-content-center flex-column">
-            <h1>Tambah Mahasiswa</h1>
-            <div class="container py-5 d-flex align-items-center justify-content-center">
-                <form method="post" action="./action/add.php" class="w-50">
-                    <label for="nama" class="form-label">Nama</label>
-                    <input type="text" class="form-control shadow-none" id="nama" name="nama" autocomplete="off" required>
-                    <label for="nim" class="form-label">NIM</label>
-                    <input type="number" class="form-control shadow-none" id="nim" name="nim" autocomplete="off" required>
-                    <label for="alamat" class="form-label">Alamat</label>
-                    <input type="text" class="form-control shadow-none" id="alamat" name="alamat" autocomplete="off" required>
-                    <div class="mt-3 d-flex flex-column justify-content-between">
-                        <button type="submit" class="btn btn-success">Tambah</button>
-                        <a href="index.php" class="btn btn-primary mt-3">Kembali</a>
-                    </div>
-                </form>
-            </div>
+<body>
+    <div class="vh-100 d-flex align-items-center justify-content-center flex-column">
+        <h1>Tambah Mahasiswa</h1>
+        <div class="container py-5 d-flex align-items-center justify-content-center">
+            <form method="post" action="./action/add.php" class="w-50">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control shadow-none" id="nama" name="nama" autocomplete="off" required>
+                <label for="nim" class="form-label">NIM</label>
+                <input type="number" class="form-control shadow-none" id="nim" name="nim" autocomplete="off" required>
+                <label for="alamat" class="form-label">Alamat</label>
+                <input type="text" class="form-control shadow-none" id="alamat" name="alamat" autocomplete="off" required>
+                <div class="mt-3 d-flex flex-column justify-content-between">
+                    <button type="submit" class="btn btn-success">Tambah</button>
+                    <a href="index.php" class="btn btn-primary mt-3">Kembali</a>
+                </div>
+            </form>
         </div>
-    </body>
+    </div>
+</body>
 
 </html>
